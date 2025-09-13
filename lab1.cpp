df03b7a8248baab706bcb4b18c747ca0184acb92
naomi23791 <cheridanemotie@gmail.com>
2025-09-13 19:04:07 +0300

    creation of the two structures

---
+++ lab1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+struct pipe
+{
+    string name;
+    float length;
+    int diameter;
+    bool repair;
+};
+struct company
+{
+    string name;
+    int workshop;
+    int workshop_in_operation;   
+    string classes;
+};
+
+int main()
+{
+    
+    return 0;
+}
